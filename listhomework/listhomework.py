2f18d4849385fbf916af01a73e279f8a3c51088b
IljaTrunov <93324206+IljaTrunov@users.noreply.github.com>
2021-12-10 22:59:52 +0200

    Update listhomework.py

--- listhomework/listhomework.py
+++ listhomework/listhomework.py
@@ -34,45 +34,35 @@ while True:
 	listik = int(input("Напиши цифру от 1 до 10 и тебе дадут список"))
 	if listik == 1:
 		print("В списке",len(slovo1), "символов")#считает длину строки
-	        print("Функция len()")
 	elif listik == 2:
 		if slovo.isalnum()==True: # Состоит ли строка из цифр или букв
-			print("Есть символы")
-                        print("Функция .isalnum()")
+			print("Есть символы"
 	elif listik == 3:
 		if slovo.swapcase()==True: # Переводит символы нижнего регистра в верхний, а верхнего – в нижний
 			print(slovo)
-			print("Функция .swapcase")
 	elif listik == 4:
 		if slovo.isspace()==True: # Состоит ли строка из неотображаемых символов
 			print("Есть пробел")
-		        print("Функция .isspace()")
 		else:
 			print("Нет пробела")
 	elif listik == 5:
 		if slovo.isalpha()==True: # Состоит ли строка из букв
 			print("В строке есть буква")
-		        print("Функция .isalpha()")
 		else:
 			print("Букв нет")
 	elif listik == 6:
 		print(slovo.capitalize()) # Переводит первый символ строки в верхний регистр, а все остальные в нижний
-	        print("Функция .capitalize()")
 	elif listik == 7:
 		print(slovo.lower()) # Преобразование строки к нижнему регистру
-	        print("Функция .lower()")
 	elif listik == 8:
 		print(slovo.upper()) # Преобразование строки к верхнему регистру
-		print("Функция .upper()")
 	elif listik == 9:
 		if slovo.istitle()==True: # Начинаются ли слова в строке с заглавной буквы
 			print("Есть заглавная буква")
-			print("Функция .istitle()")
 		else:
 			print("Нету заглавной буквы")
 	elif listik == 10:
 		if slovo.title(): # Первую букву каждого слова переводит в верхний регистр, а все остальные в нижний
-			print(slovo.title)
-		        print("Функция .title()")	
+			print(slovo.title)	
 	else:
 		print("Неверное значение")
